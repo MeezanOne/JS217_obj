1396718076f0c8d866117dd4c39fbdcfd887f38e
Meezan-onepiece <135635540+Meezan-cool@users.noreply.github.com>
2025-01-31 11:06:38 +0530

    added initial commit

---
+++ object.js
@@ -0,0 +1,18 @@
+// const person = {
+//     name: "Meezan",
+//     age: 20,
+//     hobbies: ['Chess', 'Development'],
+//     greet: ()=>{
+//         console.log(`Hi ${person.name}`);
+//     }
+// }
+
+// person.name = 'Arshlan'
+// person.age = 18
+// person.hobbies = ['Motor Vlogging', 'Gym']
+// person.greet();
+// delete person.greet
+
+// console.log(person)
+
+
